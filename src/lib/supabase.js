4a6a3627f25adf555016301a43dc92928805bc4a
Kevinaloo <worlddossy@gmail.com>
2026-06-09 12:36:40 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://xuhodkbusnprpwtkazmc.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1aG9ka2J1c25wcnB3dGthem1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTk2NzAsImV4cCI6MjA5NjEzNTY3MH0.7QLyr5YYMNeYQyEpLvk4JUSNkWTD50OjiXfkCakbIrQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false
+    detectSessionInUrl: false,
+    flowType: 'implicit'
   }
 })
